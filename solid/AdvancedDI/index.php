9f769e7eefe657de199d6680dcae1cf3d8e1e3c8
idiallo <iboudiallo84@gmail.com>
2024-10-17 21:55:29 +0200

    new dep injection with application

--- solid/AdvancedDI/index.php
+++ solid/AdvancedDI/index.php
@@ -6,10 +6,12 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 try {
 
+    /*
     $configuration = new App\Configuration(
         loggerType: 'file',
         logFilePath: __DIR__ . '/app.log',
     );
+    */
 
     $configuration = new App\Configuration(
         loggerType: 'db',
